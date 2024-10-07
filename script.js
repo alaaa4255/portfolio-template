0d01fdd2a5881845a50ac55b46e0b090738f6b1f
alaaa4255.github.io <alaaa24@nycstudents.net>
2024-10-07 12:17:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Alaa Halim"; // update the part BETWEEN the "quotes"
+let userName = "alaaa4255"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
